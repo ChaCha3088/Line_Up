f0590526ce450d5b6d8986f72dfea2f559adb00f
ChaCha3088 <cha3088@gmail.com>
2022-10-05 17:35:29 +0900

    loginfunc

--- models/user.js
+++ models/user.js
@@ -1,7 +1,37 @@
-function loginRequired(req, res, next) {
-    if (!req.user) {
-        res.redirect('/');
-        return;
+module.exports = {
+    loginRequired : function(req, res, next) {
+        if (!req.user) {
+            console.log('로그인 안되어있으면 로그아웃 후 로그인 페이지로');
+            req.logout((err) => {
+                req.session.destroy();
+                if (err) {
+                    res.redirect("/auth/kakao");
+                } else {
+                    res.clearCookie('connect.sid');
+                    res.redirect('/auth/kakao');
+                }
+            });
+            return;
+        }
+        next();
+    },
+
+    alreadyLogined: function(req, res, next) {
+        if (req.user) {
+            console.log('이미 로그인 되어있으니 루트로')
+            res.redirect('/');
+            return;
+        }
+        next();
+    }, 
+
+    accessAuth: async function(req, res, next) {
+        const result = await Sessions.findOne({'passport.user.ID': `${req.user.ID}`}).exec();
+        console.log(`result is ${result}`);
+        const objData = JSON.parse(result.session);
+        console.log(`ojbData is ${objData.passport.user.ID}`);
+        if (req.user.ID == objData) {
+            
+        }
     }
-    next();
 }
--- routes/auth.js
+++ routes/auth.js
@@ -8,10 +8,11 @@ const session = require('express-session');
 const axios = require('axios');
 const UserSchema = require('../models/schemas/auth');
 const Sessions = require('../models/schemas/session');
+const userModel = require('../models/user');
 
 router.use(passportreq.session());
 
-router.get('/kakao', passportreq.authenticate('kakao'));
+router.get('/kakao', userModel.alreadyLogined, passportreq.authenticate('kakao'));
 // 로그인 했는지 확인 먼저
 
 router.get('/kakao/callback', passportreq.authenticate('kakao', {
@@ -20,45 +21,49 @@ router.get('/kakao/callback', passportreq.authenticate('kakao', {
     res.redirect('/');
 });
 
-router.get('/kakao/logout', async (req, res)=>{
+router.get('/kakao/logout', userModel.loginRequired, async (req, res, next)=>{
     // https://kapi.kakao/com/v1/user/logout
     try {
+        //로그인 했는지 확인 먼저
+        
         const filter = {ID:`${req.user.ID}`};
         const update = { accessToken: '' };
+
+        const result = await UserSchema.findOne(filter).exec();
+        const accessToken = result.accessToken;
+
         await UserSchema.findOneAndUpdate(filter, update).exec();
         //userdb의 토큰 지워주고
 
-        const results = await Sessions.findOne({'passport.user.ID': `${req.user.ID}`}).exec();
-        console.log(`results is ${results}`);
-        const objData = JSON.parse(results.session);
-        console.log(`ojbData is ${objData.passport.user.ID}`);
+        await Sessions.findOneAndDelete({'passport.user.ID': `${req.user.ID}`}).exec();
+        // console.log(`results is ${results}`);
+        // const objData = JSON.parse(results.session);
+        // console.log(`ojbData is ${objData.passport.user.ID}`);
         //sessiondb를 지워주고
 
-        // {
-        //     _id: 'Q4YFDnt5qsyB5XcJxDz4a0wrei41m3El',
-        //     expires: 2022-10-05T03:43:01.888Z,
-        //     session: '{"cookie":{"originalMaxAge":86400000,"expires":"2022-10-05T03:43:01.888Z","secure":true,"httpOnly":true,"path":"/"},"passport":{"user":{"_id":"633aa2f3f56fdba4de9fec0a","ID":"2467587254","userName":"Cha Cha","accessToken":"6yE953NczgTVVQcrKU9V8Wbem3f4y3dEpkbm9w-GCinJXwAAAYOdDIae","createdAt":"2022-10-03T08:53:07.322Z","updatedAt":"2022-10-03T08:53:07.322Z","__v":0,"admin":"true"}}}'
-        //   }
-          
-
-        const accessToken = UserSchema.findOne(filter, update).exec();
-        // let logout = await axios({
-        //     method:'post',
-        //     url:'https://kapi.kakao.com/v1/user/logout',
-        //     headers:{
-        //     'Authorization': `Bearer ${accessToken}`
-        //     }
-        // });
+        let logout = await axios({
+            method:'post',
+            url:'https://kapi.kakao.com/v1/user/logout',
+            headers:{
+            'Authorization': `Bearer ${accessToken}`
+            }
+        });
     } catch (error) {
       console.error(error);
       res.json(error);
     }
     // 세션 정리
-    req.logout();
-    req.session.destroy();
-
-    res.redirect('/');
-  });
+    req.logout((err) => {
+        req.session.destroy();
+        if (err) {
+            res.redirect("/");
+        } else {
+            console.log('로그아웃 완료');
+            res.clearCookie('connect.sid');
+            res.redirect('/');
+        }
+	});
+});
 
 
 
